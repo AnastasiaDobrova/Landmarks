bf385a61a642a82579d463735352db85d35f5df2
AnastasiaDobrova <Anastasia.Dobrova@mail.ru>
2021-01-19 23:16:47 +0100

    missing view

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Anastasia Dobrova on 2021-01-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
